6dc88c4f1c1dcadcd757b4de30b81c49f7f3aa1a
lysiak-yevhenii <lysiak.yevhenii@gmail.com>
2023-10-28 10:00:07 +0300

    Add: fs Hint

--- learnyounode/my-first-io.js
+++ learnyounode/my-first-io.js
@@ -9,3 +9,9 @@ const buf = fs.readFileSync(process.argv[2]);
 // calling the toString() method on them.
 const str = buf.toString();
 console.log(str.split('\n').length - 1);
+
+// note you can avoid the .toString() by passing 'utf8' as the
+// second argument to readFileSync, then you'll get a String!
+//
+// fs.readFileSync(process.argv[2], 'utf8').split('\n').length - 1
+
